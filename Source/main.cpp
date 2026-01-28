00c024c386af4b2ef673502488669c61efaad41c
kd1363656 <kd1363656@st.kobedenshi.ac.jp>
2026-01-28 10:05:35 +0900

    次は画面色のクリア

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,9 +1,19 @@
 #include <Windows.h>
 #include <tchar.h>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <vector>
+#include <cassert>
 #ifdef _DEBUG
 #include <iostream>
 #endif
 
+#pragma comment(lib,"d3d12.lib")
+#pragma comment(lib,"dxgi.lib")
+
+// @brief コンソール画面にフォーマット付き文字列を表示
+// @param format フォーマット(%d とか %f とかの)
+// @remarks この関数はデバック用です。デバック時にしか動作しません
 void DebugOutputFormatString(const char* format , ...)
 {
 #ifdef _DEBUG
@@ -14,6 +24,26 @@ void DebugOutputFormatString(const char* format , ...)
 #endif
 }
 
+// 面倒だけど書かなければいけない関数
+LRESULT WindowProcedure(HWND hwnd , UINT msg , WPARAM wparam , LPARAM lparam)
+{
+	// ウィンドウが破棄されたら呼ばれる
+	if (msg == WM_DESTROY)
+	{
+		PostQuitMessage(0);	// OSに対して「もうこのアプリは終わる」と伝える
+		return 0;
+	}
+
+	return DefWindowProc(hwnd, msg, wparam, lparam);	 // 既定の処理を行う
+}
+
+static constexpr uint32_t window_width  = 1280U;
+static constexpr uint32_t window_height = 720U;
+
+ID3D12Device* _dev = nullptr;
+IDXGIFactory6* _dxgiFactory = nullptr;
+IDXGISwapChain4* _swapChain = nullptr;
+
 #ifdef _DEBUG
 int main()
 {
@@ -22,6 +52,115 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 #endif
 
 	DebugOutputFormatString("Show window test.");
-	getchar();
+	
+	// ウィンドウクラスの生成 & 登録
+	WNDCLASSEX w = {};
+	w.cbSize = sizeof(w);
+	w.lpfnWndProc = (WNDPROC)WindowProcedure; // コールバック関数の指定
+	w.lpszClassName = _T("DX12Sample");	      // アプリケーションクラス名(適当で良い)
+	w.hInstance = GetModuleHandle(nullptr);	  // ハンドルの取得
+
+	RegisterClassEx(&w); // アプリケーションクラス(ウィンドウクラスの指定を OS に伝える)
+
+	RECT wrc = { 0, 0, window_width , window_height }; // ウィンドウサイズを決める
+
+	// 関数を使ってウィンドウのサイズを補正する
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+	// ウィンドウオブジェクトの生成
+	HWND hwnd = CreateWindow(w.lpszClassName, // クラス名指定
+		_T("DX12テスト"),		// タイトルバーの文字]
+		WS_OVERLAPPEDWINDOW,		// タイトルバーと境界線があるウィンドウ
+		CW_USEDEFAULT,			// 表示X座標は OS にお任せ
+		CW_USEDEFAULT,			// 表示y座標は OS にお任せ
+		wrc.right - wrc.left,	// ウィンドウ幅
+		wrc.bottom - wrc.top,	// ウィンドウ高
+		nullptr,				// 親ウィンドウハンドル
+		nullptr,				// メニューハンドル
+		w.hInstance,			// 呼び出しアプリケーションハンドル
+	    nullptr);				// 追加パラメータ
+
+	// ウィンドウ表示
+	ShowWindow(hwnd, SW_SHOW);
+
+	//DirectX12まわり初期化
+	//フィーチャレベル列挙
+	D3D_FEATURE_LEVEL levels[] = {
+		D3D_FEATURE_LEVEL_12_1,
+		D3D_FEATURE_LEVEL_12_0,
+		D3D_FEATURE_LEVEL_11_1,
+		D3D_FEATURE_LEVEL_11_0,
+	};
+
+	auto result = CreateDXGIFactory(IID_PPV_ARGS(&_dxgiFactory));
+
+	if (FAILED(result))
+	{
+		assert(false && "DXGIFacotyr 野初期化失敗");
+		return -1;
+	}
+
+	// アダプターの列挙用
+	std::vector<IDXGIAdapter*> adapters;
+
+	// ここに特定の名前を持つアダプターオブジェクトが入る
+	IDXGIAdapter* tmpAdapter = nullptr;
+
+	for (int i = 0; _dxgiFactory->EnumAdapters(i , &tmpAdapter) != DXGI_ERROR_NOT_FOUND; ++i)
+	{
+		adapters.push_back(tmpAdapter);
+	}
+
+	for (auto adpt : adapters)
+	{
+		DXGI_ADAPTER_DESC adesc = {};
+		adpt->GetDesc(&adesc);	// アダプターの説明オブジェクト取得
+
+		std::wstring strDesc = adesc.Description;
+
+		// 探したいアダプターの名前を確認
+		if(strDesc.find(L"NVIDIA") != std::string::npos)
+		{
+			tmpAdapter = adpt;
+			break;
+		}
+	}
+
+	// Direct3D デバイスの初期化
+	D3D_FEATURE_LEVEL featureLevel;
+
+	for (auto lv : levels)
+	{
+		if (D3D12CreateDevice(nullptr , lv , IID_PPV_ARGS(&_dev)) == S_OK)
+		{
+			featureLevel = lv;
+			break;	// 生成可能なバージョンが見つかったらループを打ち切り
+		}
+	}
+
+	if (!_dev)
+	{
+		assert(false && "DXGIFacotyr 野初期化失敗");
+		return -1;
+	}
+
+
+	MSG msg = {};
+	while(true)
+	{
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		// アプリケーションが終わるときにmessageが WM_QUIT になる
+		if (msg.message == WM_QUIT) 
+		{
+			break;
+		}
+	}
+
+	// もうクラスは使わないので登録解除
 	return 0;
 }
